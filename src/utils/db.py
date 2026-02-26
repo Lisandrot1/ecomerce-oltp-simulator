0466885a0451724fbfbaa3e1cd3e13a018fe4a99
Lisandrot1 <lmlisandro3@gmail.com>
2026-02-25 19:18:01 -0500

    fix: update db.py

--- src/utils/db.py
+++ src/utils/db.py
@@ -3,13 +3,15 @@ import os
 
 
 
-# credenciales desde .env
-HOST = os.environ['NEON_DB_HOST']
-USER = os.environ['NEON_POSTGRES_USER']
-PASSWORD = os.environ['NEON_POSTGRES_PASSWORD']
-DATABASE = os.environ['NEON_DATABASE']
 
 def get_connection():
+    
+    # credenciales desde .env
+    HOST = os.environ['NEON_DB_HOST']
+    USER = os.environ['NEON_POSTGRES_USER']
+    PASSWORD = os.environ['NEON_POSTGRES_PASSWORD']
+    DATABASE = os.environ['NEON_DATABASE']
+
     engine = create_engine(
         f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}?sslmode=require',
         echo=False
